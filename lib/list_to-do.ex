427e5485ef49aad7d83ab4463476e67dacb3ed39
Martins <gabrieldossnmartins@gmail.com>
2021-06-29 14:08:48 -0300

    todoa_add1

--- lib/list_to-do.ex
+++ lib/list_to-do.ex
@@ -4,4 +4,24 @@ defmodule Todo do
   def new(name ,items \\[]) do
     %Todo{name: name, items: items}
   end
+
+  def add_item_to_board(%{name: name, items: items}, new_item ) do
+    new(name, [ new_item | items])
+  end
+
+  def remove_item_from_board(%{name: name, items: items},item_to_remove) do
+    new(name, Enum.reject(items, fn item -> item == item_to_remove end))
+  end
+
+  def mark_item_as_done(todo, done_item) do
+    todo
+      |>remove_item_from_board(done_item )
+      |>add_item_to_board(Item.done(done_item))
+  end
+
+  def mark_item_as_undone(todo, undone_item) do
+    board
+    |>remove_item_from_board(undone_item)
+    |>add_item_to_board(Item.undone(undone_item))
+  end
 end
